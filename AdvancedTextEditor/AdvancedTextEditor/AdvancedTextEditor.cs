91707871c2190e08d4380bef5ff87f1789e42be2
khaliqqq <akhaliq3@live.utm.my>
2015-09-16 17:32:38 +0800

    Initialize form UI

--- AdvancedTextEditor/AdvancedTextEditor/AdvancedTextEditor.cs
+++ AdvancedTextEditor/AdvancedTextEditor/AdvancedTextEditor.cs
@@ -10,11 +10,16 @@ using System.Windows.Forms;
 
 namespace AdvancedTextEditor
 {
-    public partial class Form1 : Form
+    public partial class AdvancedTextEditor : Form
     {
-        public Form1()
+        public AdvancedTextEditor()
         {
             InitializeComponent();
+        }
+
+        private void toolStripContainer1_TopToolStripPanel_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
